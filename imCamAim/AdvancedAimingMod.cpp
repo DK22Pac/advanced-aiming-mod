7f1fb4d0a8883c8839a320daf51ccf74ca747a9e
Valdir da Costa Júnior <junior_djjr@live.com>
2020-09-22 01:42:35 -0300

    Added weapons support
    Now .dat works by weapon ID:
    
    # AIMWEAPON_OFFSET: X    Y   Z
    -1           0.0 0.0 0.0   # Default for all weapons. Set all 0.0 to ignore it.
    22           0.22 0.0 0.0  # Pistol
    31           0.22 0.1 0.0  # M4

--- imCamAim/AdvancedAimingMod.cpp
+++ imCamAim/AdvancedAimingMod.cpp
@@ -6,163 +6,150 @@ using namespace plugin;
 
 class AdvancedAimingMod {
 public:
-    enum eWeaponAimOffset {
-        AIM_OFFSET_WEAPON_DEFAULT,
-        AIM_OFFSET_WEAPON_COLT45,
-        AIM_OFFSET_WEAPON_SILENCED,
-        AIM_OFFSET_WEAPON_DESERTEAGLE,
-        AIM_OFFSET_WEAPON_CHROMEGUN,
-        AIM_OFFSET_WEAPON_SAWNOFF,
-        AIM_OFFSET_WEAPON_SHOTGSPA,
-        AIM_OFFSET_WEAPON_MICROUZI,
-        AIM_OFFSET_WEAPON_MP5,
-        AIM_OFFSET_WEAPON_TEC9,
-        AIM_OFFSET_WEAPON_AK47,
-        AIM_OFFSET_WEAPON_M4,
-        AIM_OFFSET_WEAPON_CUNTGUN,
-        AIM_OFFSET_WEAPON_ROCKETLA,
-        AIM_OFFSET_WEAPON_HEATSEEK,
-        AIM_OFFSET_WEAPON_FLAME
-    };
+	enum eWeaponAimOffset {
+		AIM_OFFSET_WEAPON_DEFAULT,
+		AIM_OFFSET_WEAPON_COLT45,
+		AIM_OFFSET_WEAPON_SILENCED,
+		AIM_OFFSET_WEAPON_DESERTEAGLE,
+		AIM_OFFSET_WEAPON_CHROMEGUN,
+		AIM_OFFSET_WEAPON_SAWNOFF,
+		AIM_OFFSET_WEAPON_SHOTGSPA,
+		AIM_OFFSET_WEAPON_MICROUZI,
+		AIM_OFFSET_WEAPON_MP5,
+		AIM_OFFSET_WEAPON_TEC9,
+		AIM_OFFSET_WEAPON_AK47,
+		AIM_OFFSET_WEAPON_M4,
+		AIM_OFFSET_WEAPON_CUNTGUN,
+		AIM_OFFSET_WEAPON_ROCKETLA,
+		AIM_OFFSET_WEAPON_HEATSEEK,
+		AIM_OFFSET_WEAPON_FLAME
+	};
 
-    struct tAimingCamData {
-        float f0, f1, f2, f3, f4, f5, f6;
-    };
+	struct tAimingCamData {
+		float f0, f1, f2, f3, f4, f5, f6;
+	};
 
-    static tAimingCamData gData[4];
-    static CVector gOffsets[16];
+	struct tAimingOffsetData {
+		int weaponId;
+		CVector offset;
+	};
 
-    static unsigned int updateSets;
+	static tAimingCamData gData[4];
 
-    static bool rightSide;
+	static std::list<tAimingOffsetData*> offsets;
 
-    static void ReadSettings() {
-        char line[512];
-        char dummy[128];
-        float fVal;
-        FILE *file = fopen(PLUGIN_PATH("aimingSets.dat"), "r");
-        fgets(line, 512, file);
-        sscanf(line, "%s %d", dummy, &updateSets);
-        fgets(line, 512, file);
-        fgets(line, 512, file);
-        sscanf(line, "%s %f", dummy, (float *)0x8CC4B4);
-        fgets(line, 512, file);
-        sscanf(line, "%s %f", dummy, (float *)0x8CC4B8);
-        fgets(line, 512, file);
-        sscanf(line, "%s %f", dummy, &fVal);
-        patch::SetFloat(0x521632, fVal);
-        fgets(line, 512, file);
-        sscanf(line, "%s %f", dummy, (float *)0x8CC4B0);
-        fgets(line, 512, file);
-        sscanf(line, "%s %f", dummy, (float *)0x8CC4AC);
-        fgets(line, 512, file);
-        sscanf(line, "%s %f", dummy, (float *)0x8CC4A8);
-        fgets(line, 512, file);
-        sscanf(line, "%s %f", dummy, (float *)0x8CC4A0);
-        fgets(line, 512, file);
-        for (int i = 0; i < 4; i++) {
-            fgets(line, 512, file);
-            sscanf(line, "%s %f %f %f %f %f %f %f", dummy, &gData[i].f0, &gData[i].f1, &gData[i].f2, &gData[i].f3, &gData[i].f4,
-                &gData[i].f5, &gData[i].f6);
-        }
-        memcpy((void *)0x8CC4C0, gData, 112);
-        fgets(line, 512, file);
-        for (int i = 0; i < 16; i++) {
-            fgets(line, 512, file);
-            sscanf(line, "%s %f %f %f", dummy, &gOffsets[i].x, &gOffsets[i].y, &gOffsets[i].z);
-        }
-        fclose(file);
-    }
+	static unsigned int updateSets;
 
-    static void _fastcall MyProcess_AimWeapon(CCam *cam, int, CVector const &vec, float arg3, float arg4, float arg5) {
-        CPed *playa = FindPlayerPed();
-        if (playa && !playa->m_nPedFlags.bInVehicle) {
-            int aimTypeId = -1;
-            switch (playa->m_aWeapons[playa->m_nActiveWeaponSlot].m_Type) {
-            case WEAPON_PISTOL:
-                aimTypeId = AIM_OFFSET_WEAPON_COLT45;
-                break;
-            case WEAPON_PISTOL_SILENCED:
-                aimTypeId = AIM_OFFSET_WEAPON_SILENCED;
-                break;
-            case WEAPON_DESERT_EAGLE:
-                aimTypeId = AIM_OFFSET_WEAPON_DESERTEAGLE;
-                break;
-            case WEAPON_SHOTGUN:
-                aimTypeId = AIM_OFFSET_WEAPON_CHROMEGUN;
-                break;
-            case WEAPON_SAWNOFF:
-                aimTypeId = AIM_OFFSET_WEAPON_SAWNOFF;
-                break;
-            case WEAPON_SPAS12:
-                aimTypeId = AIM_OFFSET_WEAPON_SHOTGSPA;
-                break;
-            case WEAPON_MICRO_UZI:
-                aimTypeId = AIM_OFFSET_WEAPON_MICROUZI;
-                break;
-            case WEAPON_MP5:
-                aimTypeId = AIM_OFFSET_WEAPON_MP5;
-                break;
-            case WEAPON_TEC9:
-                aimTypeId = AIM_OFFSET_WEAPON_TEC9;
-                break;
-            case WEAPON_AK47:
-                aimTypeId = AIM_OFFSET_WEAPON_AK47;
-                break;
-            case WEAPON_M4:
-                aimTypeId = AIM_OFFSET_WEAPON_M4;
-                break;
-            case WEAPON_COUNTRYRIFLE:
-                aimTypeId = AIM_OFFSET_WEAPON_CUNTGUN;
-                break;
-            case WEAPON_ROCKET:
-                aimTypeId = AIM_OFFSET_WEAPON_ROCKETLA;
-                break;
-            case WEAPON_ROCKET_HS:
-                aimTypeId = AIM_OFFSET_WEAPON_HEATSEEK;
-                break;
-            case WEAPON_FTHROWER:
-                aimTypeId = AIM_OFFSET_WEAPON_FLAME;
-                break;
-            }
-            if (aimTypeId != -1) {
-                CVector offset = gOffsets[aimTypeId];
+	static bool rightSide;
 
-                // if rightSide flag enabled
-                if (rightSide)
-                    offset.x *= -3.0f;
+	static CVector defaultOffset;
 
-                CallMethod<0x521500, CCam *, CVector const&>(cam, playa->TransformFromObjectSpace(offset), arg3, arg4, arg5);
-            }
-            else
-                CallMethod<0x521500, CCam *, CVector const&>(cam, vec, arg3, arg4, arg5);
-        }
-        else
-            CallMethod<0x521500, CCam *, CVector const&>(cam, vec, arg3, arg4, arg5);
-    }
+	static CVector GetAimingOffsetData(int weaponId) {
+		// Find weapon
+		for (auto data : offsets) {
+			if (data->weaponId == weaponId) return data->offset;
+		}
+		// Find default settings
+		for (auto data : offsets) {
+			if (data->weaponId == -1) return data->offset;
+		}
+		// Fall off
+		CVector offset;
+		offset.x = 0.0f; offset.y = 0.0f; offset.z = 0.0f;
+		return offset;
+	}
 
-    AdvancedAimingMod() {
-        // inject our function
-        patch::RedirectCall(0x527A95, MyProcess_AimWeapon);
-        // read settings
-        ReadSettings();
+	static void ReadSettings() {
+		char line[512];
+		char dummy[128];
+		float fVal;
+		FILE *file = fopen(PLUGIN_PATH("aimingSets.dat"), "r");
+		fgets(line, 512, file);
+		sscanf(line, "%s %d", dummy, &updateSets);
+		fgets(line, 512, file);
+		fgets(line, 512, file);
+		sscanf(line, "%s %f", dummy, (float *)0x8CC4B4);
+		fgets(line, 512, file);
+		sscanf(line, "%s %f", dummy, (float *)0x8CC4B8);
+		fgets(line, 512, file);
+		sscanf(line, "%s %f", dummy, &fVal);
+		patch::SetFloat(0x521632, fVal);
+		fgets(line, 512, file);
+		sscanf(line, "%s %f", dummy, (float *)0x8CC4B0);
+		fgets(line, 512, file);
+		sscanf(line, "%s %f", dummy, (float *)0x8CC4AC);
+		fgets(line, 512, file);
+		sscanf(line, "%s %f", dummy, (float *)0x8CC4A8);
+		fgets(line, 512, file);
+		sscanf(line, "%s %f", dummy, (float *)0x8CC4A0);
+		fgets(line, 512, file);
+		for (int i = 0; i < 4; i++) {
+			fgets(line, 512, file);
+			sscanf(line, "%s %f %f %f %f %f %f %f", dummy, &gData[i].f0, &gData[i].f1, &gData[i].f2, &gData[i].f3, &gData[i].f4,
+				   &gData[i].f5, &gData[i].f6);
+		}
+		memcpy((void *)0x8CC4C0, gData, 112);
+		fgets(line, 512, file);
+		CVector offset;
+		int weaponId = 0;
+		for (int i = 0; i < 16; i++) {
+			fgets(line, 512, file);
+			if (sscanf(line, "%i %f %f %f", &weaponId, &offset.x, &offset.y, &offset.z) == 4) {
+				tAimingOffsetData *data = new tAimingOffsetData;
+				data->weaponId = weaponId;
+				data->offset = offset;
+				offsets.push_back(data);
+			}
+		}
+		fclose(file);
+	}
 
-        Events::gameProcessEvent += []() {
-            // check for key-press
-            if (KeyPressed('Z'))
-                rightSide = false;
-            else if (KeyPressed('X'))
-                rightSide = true;
+	static void _fastcall MyProcess_AimWeapon(CCam *cam, int, CVector const &vec, float arg3, float arg4, float arg5) {
+		CPed *playa = FindPlayerPed();
+		if (playa && !playa->m_nPedFlags.bInVehicle) {
 
-            // update settings
-            if (updateSets)
-                ReadSettings();
-        };
-    }
+			CVector offset = GetAimingOffsetData(playa->m_aWeapons[playa->m_nActiveWeaponSlot].m_nType);
+			
+			if (offset.x != 0 || offset.y != 0 || offset.z != 0) {
+
+				// if rightSide flag enabled
+				if (rightSide)
+					offset.x *= -3.0f;
+
+				CallMethod<0x521500, CCam *, CVector const&>(cam, playa->TransformFromObjectSpace(offset), arg3, arg4, arg5);
+			}
+			else
+				CallMethod<0x521500, CCam *, CVector const&>(cam, vec, arg3, arg4, arg5);
+		}
+		else
+			CallMethod<0x521500, CCam *, CVector const&>(cam, vec, arg3, arg4, arg5);
+	}
+
+	AdvancedAimingMod() {
+		// inject our function
+		patch::RedirectCall(0x527A95, MyProcess_AimWeapon);
+		// read settings
+		ReadSettings();
+
+		Events::gameProcessEvent += []() {
+			// check for key-press
+			if (KeyPressed('Z'))
+				rightSide = false;
+			else if (KeyPressed('X'))
+				rightSide = true;
+
+			// update settings
+			if (updateSets)
+				ReadSettings();
+		};
+	}
 };
 
 AdvancedAimingMod::tAimingCamData AdvancedAimingMod::gData[4];
-CVector AdvancedAimingMod::gOffsets[16];
+
+std::list<AdvancedAimingMod::tAimingOffsetData*> AdvancedAimingMod::offsets;
+
+CVector AdvancedAimingMod::defaultOffset;
 
 unsigned int AdvancedAimingMod::updateSets;
 
